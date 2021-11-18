5014f1ceb64e8aa3d41b2a6949f0147e5797c19d
Ajay Kayyam <saiajay220@gmail.com>
2021-11-18 20:09:44 +0530

    Using Context and Adding Reducer

--- src/components/Layouts/HeaderCartButton.js
+++ src/components/Layouts/HeaderCartButton.js
@@ -1,14 +1,19 @@
 import React from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
+  const ctx = React.useContext(CartContext);
+  const numberofcartItems = ctx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>0</span>
+      <span className={classes.badge}>{numberofcartItems}</span>
     </button>
   );
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,10 +1,35 @@
+import React from "react";
 import CartContext from "./cart-context";
+
+const defaultcartitems = {
+  items: [],
+  totalamount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = state.items.concat(action.value);
+    const updatedTotalamount =
+      state.totalamount + action.value.price * action.value.amount;
+    return {
+      items: updatedItems,
+      totalamount: updatedTotalamount,
+    };
+  }
+  return defaultcartitems;
+};
+
 const CartProvider = (props) => {
-  const addItemHandler = (item) => {}; //adding items into cart
+  const [cartState, dispatchCart] = React.useReducer(
+    cartReducer,
+    defaultcartitems
+  );
+  const addItemHandler = (item) => {
+    dispatchCart({ type: "ADD_ITEM", value: item });
+  }; //adding items into cart
   const removeItemHandler = (id) => {}; //removing items into cart
   const cartcontext = {
-    items: [],
-    totalamount: 0,
+    items: cartState.items,
+    totalamount: cartState.amount,
     addItem: addItemHandler,
     removeItem: removeItemHandler,
   }; //default value for cartcontext
