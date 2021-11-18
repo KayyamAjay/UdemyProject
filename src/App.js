f3964a5bc7883bb8d0590269888310fee0d00710
Ajay Kayyam <saiajay220@gmail.com>
2021-11-18 11:18:46 +0530

    Adding Meals Summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Layouts/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Meals />
     </React.Fragment>
   );
 }
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <React.Fragment>
+      <MealsSummary />
+    </React.Fragment>
+  );
+};
+
+export default Meals;
