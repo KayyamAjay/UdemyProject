e4ebe78afe8908f58c2c6637fcac09ff4fa40d2b
Ajay Kayyam <saiajay220@gmail.com>
2021-11-19 19:13:23 +0530

    using useEffect for animation

--- src/components/Layouts/HeaderCartButton.js
+++ src/components/Layouts/HeaderCartButton.js
@@ -3,12 +3,28 @@ import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
+  const [btnHigh, setbtnHigh] = React.useState(false);
   const ctx = React.useContext(CartContext);
   const numberofcartItems = ctx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+  const btnclasses = `${classes.button} ${btnHigh && classes.bump}`;
+
+  React.useEffect(() => {
+    if (ctx.items.length === 0) {
+      return;
+    }
+    setbtnHigh(true);
+    const timer = setTimeout(() => {
+      setbtnHigh(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnclasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
